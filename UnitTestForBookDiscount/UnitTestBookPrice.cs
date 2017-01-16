a556dd18672ed81b0e309ca2e56c29bc1f0cb248
TONY <TONY@15000651TONY>
2017-01-16 23:23:02 +0800

    追加 "Scenario: 第一集買了一本，第二三集各買了兩本" 的 UnitTest, 並完成該Scenario

--- UnitTestForBookDiscount/UnitTestBookPrice.cs
+++ UnitTestForBookDiscount/UnitTestBookPrice.cs
@@ -128,5 +128,27 @@ namespace UnitTestBookDiscount
             //Assert
             Assert.AreEqual(expect, actual);
         }
+
+        [TestMethod]
+        public void 第一集買了一本第二三集各買了兩本()
+        {
+            //Arrange
+            var order = new List<Book>()
+            {
+                new Book() { Name = "Harry Potter Vol.1", Volume = 1, Price = 100 },
+                new Book() { Name = "Harry Potter Vol.2", Volume = 2, Price = 100 },
+                new Book() { Name = "Harry Potter Vol.2", Volume = 2, Price = 100 },
+                new Book() { Name = "Harry Potter Vol.3", Volume = 3, Price = 100 },
+                new Book() { Name = "Harry Potter Vol.3", Volume = 3, Price = 100 },
+            };
+
+            var expect = 460;
+
+            //Act
+            var actual = new BookPricing().CalculatePrice(order);
+
+            //Assert
+            Assert.AreEqual(expect, actual);
+        }
     }
 }
