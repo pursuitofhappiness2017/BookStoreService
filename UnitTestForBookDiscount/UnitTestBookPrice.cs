41b8b1447085ee57558d41c310bea02a246976a0
TONY <TONY@15000651TONY>
2017-01-16 22:14:26 +0800

    追加第二個Scenario的UnitTest

--- UnitTestForBookDiscount/UnitTestBookPrice.cs
+++ UnitTestForBookDiscount/UnitTestBookPrice.cs
@@ -25,5 +25,24 @@ namespace UnitTestBookDiscount
             //Assert
             Assert.AreEqual(expect, actual);
         }
+
+        [TestMethod]
+        public void 第一集買了一本第二集也買了一本()
+        {
+            //Arrange
+            var order = new List<Book>()
+            {
+                new Book() { Name = "Harry Potter Vol.1", Volume = 1, Price = 100 },
+                new Book() { Name = "Harry Potter Vol.2", Volume = 2, Price = 100 },
+            };
+
+            var expect = 190;
+
+            //Act
+            var actual = new BookPricing().CalculatePrice(order);
+
+            //Assert
+            Assert.AreEqual(expect, actual);
+        }
     }
 }
